74a4064d8e1d6765fcd9cb652ed118bfae57bf24
mankop <mankop@spse-po.sk>
2020-06-26 10:44:29 +0200

    Changed model Subpage

--- app/Subpage.php
+++ app/Subpage.php
@@ -6,6 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subpage extends Model
 {
+
+    protected $fillable = [
+        'title',
+        'description',
+        'image',
+        'title_link',
+    ];
+
     public function posts(){
         return $this->hasMany(Post::class)->orderBy('created_at', 'DESC')->where('active',true);
     }
