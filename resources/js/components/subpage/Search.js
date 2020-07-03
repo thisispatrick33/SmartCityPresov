c29828be33b3c51d4cd9aaae23cded00cb2c2955
Patrik Mosorják <patrik.mosorjak345@gmail.com>
2020-07-03 09:04:57 +0200

    search-design

--- resources/js/components/subpage/Search.js
+++ resources/js/components/subpage/Search.js
@@ -1,25 +1,21 @@
 import React, {useEffect, useState} from "react";
 import $ from 'jquery';
 import {ProjectOutlook} from "./outlook/ProjectOutlook";
+import {NewsOutlook} from "./outlook/NewsOutlook";
 import {Loader} from "../Utillities";
 export const Search = ({close = f => f, searchFetchData = f => f, allSubpageData, getPost = f => f}) => {
 
     const [searchInput, setSearchInput] = useState("");
     const [firstTime, setFirstTime] = useState(true);
-
     useEffect(() => {
-        $('.project-details-frame').fadeIn();
-        $('.project-details-frame .project-content').animate({
-            marginTop: '10vh',
-            easing: 'easeInOutCirc'
-        },1000);
-        $('.gallery').css("maxHeight",$('.project-content .content').height());
+        $('.searchbar-frame').fadeIn();
+        $('body,html').css('overflow', 'hidden');
         if(firstTime){
             searchFetchData();
             console.log("FirstTime");
             setFirstTime(false);
         }
-    }, [allSubpageData]);
+    }, []);
 
     if(allSubpageData == null || allSubpageData["/mobilita"] == undefined || allSubpageData["/zivotne_prostredie"] == undefined || allSubpageData["/digitalne_mesto"] == undefined || allSubpageData["/energia"] == undefined){
         console.log("loader");
@@ -27,60 +23,68 @@ export const Search = ({close = f => f, searchFetchData = f => f, allSubpageData
         return <Loader/>
     }
     else {
-        console.log("search");
         return (
-            <div className={"project-details-frame row mx-0 p-0 m-0 justify-content-center"} style={{display : `none`}}>
-                <div className="project-content col-xl-9 col-lg-9 col-md-10 col-12 row mx-0 shadow p-0 justify-content-start align-items-start">
-                    <div className="gallery m-0 col-xl-2 col-lg-2 col-12 order-xl-1 order-lg-1 order-2 row mx-0 p-0 " style={{overflowY : 'scroll'}}>
-                        <div className="col-12 p-0 mt-5 title">
-                            <h3 className={"text-center"}>galéria</h3>
-                        </div>
-
-                    </div>
-                    <div className="content m-0 shadow col-xl-10 col-lg-10 col-12 order-xl-2 order-lg-2 order-1 row mx-0 justify-content-end p-0 align-items-start">
-                        <div className="col-12 row mx-0 p-0">
-                            <div className="col-12 row mx-0 mt-5 px-5 title p-0 justify-content-xl-start justify-content-lg-start justify-content-md-start justify-content-end">
-                                <h2 className={"title p-xl-2 p-lg-2 p-md-2 px-0 col-xl-11 col-lg-11 col-md-11 col-12 order-xl-1 order-lg-1 order-md-1 order-2 mb-3"}>SearchBar</h2>
-                                <span onClick={() => close()} className={"col-1 order-xl-2 order-lg-2 order-md-2 order-1 p-0"}>
-                                <svg className={"col-12 p-0 close-button"} enableBackground="new 0 0 357 357" version="1.1" viewBox="0 0 357 357" space="preserve" xmlns="http://www.w3.org/2000/svg">
-                                        <polygon points="357 35.7 321.3 0 178.5 142.8 35.7 0 0 35.7 142.8 178.5 0 321.3 35.7 357 178.5 214.2 321.3 357 357 321.3 214.2 178.5"/>
-                                </svg>
-                            </span>
-                            </div>
-                            <input onChange={e => setSearchInput(e.target.value)}/>
-                            {
-                                console.log(allSubpageData)
-                            }
-                            {
-                                allSubpageData["/mobilita"].posts.map((current) => {
-                                    return(
-                                        (current.title.toUpperCase().includes(searchInput.toUpperCase()) || current.description.toUpperCase().includes(searchInput.toUpperCase())) && <ProjectOutlook post={current} getPost={id => getPost(id)}/>
-                                    )
-                                })
-                            }
-                            {
-                                allSubpageData["/zivotne_prostredie"].posts.map((current) => {
-                                    return(
-                                        (current.title.toUpperCase().includes(searchInput.toUpperCase()) || current.description.toUpperCase().includes(searchInput.toUpperCase())) && <ProjectOutlook post={current} getPost={id => getPost(id)}/>
-                                    )
-                                })
-                            }
-                            {
-                                allSubpageData["/digitalne_mesto"].posts.map((current) => {
-                                    return(
-                                        (current.title.toUpperCase().includes(searchInput.toUpperCase()) || current.description.toUpperCase().includes(searchInput.toUpperCase())) && <ProjectOutlook post={current} getPost={id => getPost(id)}/>
-                                    )
-                                })
-                            }
-                            {
-                                allSubpageData["/energia"].posts.map((current) => {
-                                    return(
-                                        (current.title.toUpperCase().includes(searchInput.toUpperCase()) || current.description.toUpperCase().includes(searchInput.toUpperCase())) && <ProjectOutlook post={current} getPost={id => getPost(id)}/>
-                                    )
-                                })
-                            }
-                        </div>
-                    </div>
+            <div className={"searchbar-frame row mx-0 p-0 m-0 justify-content-center align-items-start position-fixed"}>
+                <input onChange={e => setSearchInput(e.target.value)} className={'col-xl-8 py-3 px-4 mt-4 shadow-sm border-0 background-white'} placeholder={'Zadajte vyhľadávaný výraz'}/>
+                <div className={`row col-xl-10 col-lg-11 col-md-11 col-10 | my-4 p-0`}>
+                    {
+                        allSubpageData["/mobilita"].posts.filter(({done}) => done == 0).map((current) => {
+                            return(
+                                (current.title.toUpperCase().includes(searchInput.toUpperCase()) || current.description.toUpperCase().includes(searchInput.toUpperCase())) && <div className='col-3 mx-0 row'><NewsOutlook post={current} getPost={id => getPost(id)}/></div>
+                            )
+                        })
+                    }
+                    {
+                        allSubpageData["/zivotne_prostredie"].posts.filter(({done}) => done == 0).map((current) => {
+                            return(
+                                (current.title.toUpperCase().includes(searchInput.toUpperCase()) || current.description.toUpperCase().includes(searchInput.toUpperCase())) && <div className='col-3 mx-0 row'><NewsOutlook post={current} getPost={id => getPost(id)}/></div>
+                            )
+                        })
+                    }
+                    {
+                        allSubpageData["/digitalne_mesto"].posts.filter(({done}) => done == 0).map((current) => {
+                            return(
+                                (current.title.toUpperCase().includes(searchInput.toUpperCase()) || current.description.toUpperCase().includes(searchInput.toUpperCase())) && <div className='col-3 mx-0 row'><NewsOutlook post={current} getPost={id => getPost(id)}/></div>
+                            )
+                        })
+                    }
+                    {
+                        allSubpageData["/energia"].posts.filter(({done}) => done == 0).map((current) => {
+                            return(
+                                (current.title.toUpperCase().includes(searchInput.toUpperCase()) || current.description.toUpperCase().includes(searchInput.toUpperCase())) && <div className='col-3 mx-0 row'><NewsOutlook post={current} getPost={id => getPost(id)}/></div>
+                            )
+                        })
+                    }       
+                </div>
+                <div className={`row col-xl-10 col-lg-11 col-md-11 col-10 | my-4 p-0`}>
+                    {
+                        allSubpageData["/mobilita"].posts.filter(({done}) => done == 1).map((current) => {
+                            return(
+                                (current.title.toUpperCase().includes(searchInput.toUpperCase()) || current.description.toUpperCase().includes(searchInput.toUpperCase())) && <div className='col-3 mx-0 row'><ProjectOutlook post={current} getPost={id => getPost(id)}/></div>
+                            )
+                        })
+                    }
+                    {
+                        allSubpageData["/zivotne_prostredie"].posts.filter(({done}) => done == 1).map((current) => {
+                            return(
+                                (current.title.toUpperCase().includes(searchInput.toUpperCase()) || current.description.toUpperCase().includes(searchInput.toUpperCase())) && <div className='col-3 mx-0 row'><ProjectOutlook post={current} getPost={id => getPost(id)}/></div>
+                            )
+                        })
+                    }
+                    {
+                        allSubpageData["/digitalne_mesto"].posts.filter(({done}) => done == 1).map((current) => {
+                            return(
+                                (current.title.toUpperCase().includes(searchInput.toUpperCase()) || current.description.toUpperCase().includes(searchInput.toUpperCase())) && <div className='col-3 mx-0 row'><ProjectOutlook post={current} getPost={id => getPost(id)}/></div>
+                            )
+                        })
+                    }
+                    {
+                        allSubpageData["/energia"].posts.filter(({done}) => done == 1).map((current) => {
+                            return(
+                                (current.title.toUpperCase().includes(searchInput.toUpperCase()) || current.description.toUpperCase().includes(searchInput.toUpperCase())) && <div className='col-3 mx-0 row'><ProjectOutlook post={current} getPost={id => getPost(id)}/></div>
+                            )
+                        })
+                    }
                 </div>
             </div>
         );
